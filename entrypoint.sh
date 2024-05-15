dab526a3b818e7c8b0f0c1c6b53a1d1d8ce625ee
Patrick Bronneberg <patrick.bronneberg@philips.com>
2024-05-15 11:50:13 +0200

    Capture output variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,11 @@ export BUILD_PATH=./$2
 mkdir -p ${BUILD_PATH}
 chmod 777 ${BUILD_PATH}
 echo "Building using builder --config $1"
-builder --config $1 --output-path ${BUILD_PATH}/otelcolaction
-chmod 755 ${BUILD_PATH}/otelcolaction
+builder --config $1 --output-path ${BUILD_PATH}
+chmod 755 ${BUILD_PATH}
 
-# time=$(date)
-# echo "time=$time" >> $GITHUB_OUTPUT
+CONFIG_PATH=$1
+DIST_NAME=$(cat $CONFIG_PATH | yq '.dist.name')
+
+echo "collector-file='${BUILD_PATH}/${DIST_NAME}'" >> $GITHUB_OUTPUT
+
