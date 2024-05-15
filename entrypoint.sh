1d7b5353e3f482fd364003c7560ecee70fab7e4a
Patrick Bronneberg <patrick.bronneberg@philips.com>
2024-05-15 17:27:05 +0200

    Fix deprecated output path argument

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,16 @@
 export CGO_ENABLED=0
 export GOFLAGS=-buildvcs=false
 export BUILD_PATH=./$2
-mkdir -p ${BUILD_PATH}
-chmod 777 ${BUILD_PATH}
-echo "Building using builder --config $1"
-builder --config $1 --output-path ${BUILD_PATH}
-chmod 755 ${BUILD_PATH}
-
 CONFIG_PATH=$1
 DIST_NAME=$(cat $CONFIG_PATH | yq '.dist.name')
+
+#Override the output_path of the configuration to the provided build_path
+yq -i ".dist.output_path = \"${BUILD_PATH}\"" ${CONFIG_PATH} 
+
+mkdir -p ${BUILD_PATH}
+chmod 777 ${BUILD_PATH}
+echo "Building using config: ${CONFIG_PATH}"
+builder --config ${CONFIG_PATH}
+chmod 755 ${BUILD_PATH}
 
 echo "collector-file='${BUILD_PATH}/${DIST_NAME}'" >> $GITHUB_OUTPUT
