614d8953f1c31745adeecf84fe34e3324f772733
Patrick Bronneberg <patrick.bronneberg@philips.com>
2024-05-15 08:53:55 +0200

    Use globally installed builder

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ export GOFLAGS=-buildvcs=false
 export BUILD_PATH=./$2
 mkdir -p ${BUILD_PATH}
 chmod 777 ${BUILD_PATH}
-echo "Building using /builder --config $1"
-/builder --config $1 --output-path ${BUILD_PATH}/otelcolaction
+echo "Building using builder --config $1"
+builder --config $1 --output-path ${BUILD_PATH}/otelcolaction
 chmod 755 ${BUILD_PATH}/otelcolaction
 
 # time=$(date)
